9adc2ba2db989a4d58873370e672347d098a7334
AlexM <134806248+oceanbytes12@users.noreply.github.com>
2023-07-19 12:18:05 -0700

    Added Player effects and movement smoothing
    - Added a sate system in Player to track when they change between animations
    - Also added lerp function so player slides a bit before stopping

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -8,6 +8,12 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+@onready var land_effect = preload("res://Scenes/Effects/land/land_effect.tscn")
+@onready var jump_effect = preload("res://Scenes/Effects/Jump/JumpEffect.tscn")
+
+
+var state = "idle"
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -23,23 +29,47 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = lerp(velocity.x, 0.0, 0.2)
 	
 	
-	# Handling animation
+	# Handling animations and state
+	# Check for facing
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
 	
-	if velocity == Vector2.ZERO and is_on_floor():
+	# Set animations
+	if direction == 0 and is_on_floor():
 		$AnimatedSprite2D.play("Idle")
+		state = handle_state_change(state, "idle")
 	elif is_on_floor():
 		$AnimatedSprite2D.play("Walking")
-	
+		state = handle_state_change(state, "walking")
 	if not is_on_floor() and velocity.y <= 0:
 		$AnimatedSprite2D.play("Jump")
+		state = handle_state_change(state, "soaring")
 	elif not is_on_floor():
 		$AnimatedSprite2D.play("Fall")
+		state = handle_state_change(state, "falling")
 
+	# Move 
 	move_and_slide()
+	
+
+func handle_state_change(old_state, new_state):
+	var was_grounded = (old_state == "idle" or old_state == "walking")
+	var is_grounded = (new_state == "idle" or new_state == "walking")
+	if was_grounded and new_state == "soaring":
+		var dust = jump_effect.instantiate()
+		get_parent().get_parent().add_child(dust)
+		dust.global_position = global_position
+		dust.flip_h = !$AnimatedSprite2D.flip_h 
+	if old_state == "falling" and is_grounded:
+		var dust = land_effect.instantiate()
+		get_parent().get_parent().add_child(dust)
+		dust.global_position = global_position
+		dust.flip_h = !$AnimatedSprite2D.flip_h 
+		
+	return new_state
+	
