53420794a1d5aad171e3ee6fe9febf06143ae12b
AlexM <134806248+oceanbytes12@users.noreply.github.com>
2023-07-19 12:46:01 -0700

    Added coyote time and jump saving
    - Added coyote time to give players a chance to jump a spit second after they fall off an edge.
    - Also added jump caching to save jump presses while the player is still in the air.
    - Was a good opportunity to set up exported variables to allow fine tuning.
    - Learned more about the timer system in Godot

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,39 +1,46 @@
 extends CharacterBody2D
 
+@onready var land_effect = preload("res://Scenes/Effects/land/land_effect.tscn")
+@onready var jump_effect = preload("res://Scenes/Effects/Jump/JumpEffect.tscn")
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+@export var movement_speed = 200.0
+@export var jump_power = -400.0
+@export var coyote_time = 0.15
+@export var saved_jump_time = 1.0
+
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-@onready var land_effect = preload("res://Scenes/Effects/land/land_effect.tscn")
-@onready var jump_effect = preload("res://Scenes/Effects/Jump/JumpEffect.tscn")
-
-
 var state = "idle"
+var saved_jump = false
+var coyote_save = false
+var coyote_timer
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	# Handle Jump. If in air save the jump input, so he jumps as soon as he hits the ground
+	if Input.is_action_just_pressed("ui_accept"):
+		saved_jump_time_handler()
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if saved_jump and (is_on_floor() or coyote_save):
+		velocity.y = jump_power
+		saved_jump = false
+		coyote_save = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * movement_speed
 	else:
 		velocity.x = lerp(velocity.x, 0.0, 0.2)
 	
 	
-	# Handling animations and state
-	# Check for facing
+	# Set image facing
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif velocity.x < 0:
@@ -69,7 +76,18 @@ func handle_state_change(old_state, new_state):
 		var dust = land_effect.instantiate()
 		get_parent().get_parent().add_child(dust)
 		dust.global_position = global_position
-		dust.flip_h = !$AnimatedSprite2D.flip_h 
+		dust.flip_h = !$AnimatedSprite2D.flip_h
+	if was_grounded and new_state == "falling":
+		coyote_time_handler()
 		
 	return new_state
 	
+func coyote_time_handler():
+	coyote_save = true
+	await get_tree().create_timer(coyote_time).timeout
+	coyote_save = false
+
+func saved_jump_time_handler():
+	saved_jump = true
+	await get_tree().create_timer(coyote_time).timeout
+	saved_jump = false
